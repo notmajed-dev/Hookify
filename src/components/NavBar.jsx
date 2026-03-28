14ff51467bc24ba8b9cfc70db96d154db5df930c
Majed <notmajed.js@gmail.com>
2026-03-28 16:45:11 +0530

    Small tweaks

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,22 +2,31 @@ import * as LucideIcons from "lucide-react";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-between items-center bg-black p-4">
-      <h1 className="text-white font-mono text-3xl cursor-pointer font-bold flex items-center">
-        <LucideIcons.CircleParking size={40} />
-        Pomo<span className="text-green-600">Focus</span>
-      </h1>
-      <ul className="flex gap-6 text-white  ">
-        <li className="hover:bg-green-600 rounded-full p-3 ">
-          <LucideIcons.Timer size={28} />
-        </li>
-        <li className="hover:bg-green-600 rounded-full p-3 ">
-          <LucideIcons.ListTodo size={28} />
-        </li>
-        <li className="hover:bg-green-600 rounded-full p-3 ">
-          <LucideIcons.BarChart size={28} />
-        </li>
-      </ul>
+    <div className="bg-white border-b-4 border-black border-double px-6 py-4">
+      <div className="max-w-6xl mx-auto flex justify-between items-center">
+
+        {/* Logo */}
+        <h1 className="text-black font-mono text-3xl font-bold flex items-center gap-2 cursor-pointer">
+          <LucideIcons.CircleParking size={36} />
+          Pomo<span className="text-black">Focus</span>
+        </h1>
+
+        {/* Icons */}
+        <ul className="flex gap-4">
+          <li className="p-3 border-4 border-black border-double rounded-full hover:bg-black hover:text-white transition cursor-pointer">
+            <LucideIcons.Timer size={24} />
+          </li>
+
+          <li className="p-3 border-4 border-black border-double rounded-full hover:bg-black hover:text-white transition cursor-pointer">
+            <LucideIcons.ListTodo size={24} />
+          </li>
+
+          <li className="p-3 border-4 border-black border-double rounded-full hover:bg-black hover:text-white transition cursor-pointer">
+            <LucideIcons.BarChart size={24} />
+          </li>
+        </ul>
+
+      </div>
     </div>
   );
 };
